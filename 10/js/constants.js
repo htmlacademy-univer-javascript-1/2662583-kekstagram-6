df7d503937322b3e7d2d4c3df107f686932dbd94
Keks <keks@htmlacademy.ru>
2025-12-22 11:43:37 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/constants.js
@@ -0,0 +1,59 @@
+const NAMES = [
+  'Иван',
+  'Дмитрий',
+  'Мария',
+  'Максим',
+  'Виктор',
+  'Юлия',
+  'Артём',
+  'Ксения',
+  'Александр',
+  'Матвей',
+  'Арина',
+];
+
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const DESCRIPTIONS = [
+  'В семейном кругу',
+  'Наконец-то отпуск!',
+  'Вся красота мира в одном кадре',
+  'Невероятное место!',
+  'Уютный вечер',
+  'Новогодняя сказка',
+  'Мечты сбываются!',
+  'Сегодня на деловом',
+  'Вечер с друзьями',
+];
+
+const POSTS_COUNT = 25;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
+const MIN_AVATAR = 1;
+const MAX_AVATAR = 6;
+const MIN_MESSAGES_COUNT = 1;
+const MAX_MESSAGES_COUNT = 2;
+const LOAD_COMMENTS_COUNT = 5;
+const HASHTAG_REGEX = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_HASHTAGS = 5;
+const MAX_COMMENT_LENGTH = 140;
+const SERVER_URL = 'https://29.javascript.htmlacademy.pro/kekstagram';
+const GET_DATA_URL = `${SERVER_URL}/data`;
+
+const ERROR_MESSAGES = {
+  GET: 'Не удалось загрузить фотографии. Попробуйте обновить страницу',
+  SEND: 'Не удалось отправить форму. Попробуйте ещё раз'
+};
+
+
+export {NAMES, MESSAGES, DESCRIPTIONS, POSTS_COUNT, MIN_LIKES, MAX_LIKES, MIN_COMMENTS, MAX_COMMENTS,
+  MIN_AVATAR, MAX_AVATAR, MIN_MESSAGES_COUNT,MAX_MESSAGES_COUNT, LOAD_COMMENTS_COUNT, HASHTAG_REGEX, MAX_HASHTAGS, MAX_COMMENT_LENGTH, SERVER_URL, GET_DATA_URL, ERROR_MESSAGES};
